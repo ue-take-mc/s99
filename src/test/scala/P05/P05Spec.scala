7a5fa49eeedbb006a918b5d70bc6d604504e256c
UEDA Takeshi <takeshi@freemind.co.jp>
2016-06-09 18:41:12 +0900

    P05 test by scalatest

---
+++ src/test/scala/P05/P05Spec.scala
@@ -0,0 +1,23 @@
+package P05
+
+import org.scalatest.{DiagrammedAssertions, FunSpec}
+
+/**
+  * Created by ueda on 2016/06/09.
+  */
+class P05Spec extends FunSpec with DiagrammedAssertions {
+  describe("P05") {
+    it ("for empty list") {
+      assert(P05.reverse(List[Int]()).isEmpty)
+      assert(P05.reverseByFoldRight(List[Int]()).isEmpty)
+    }
+    it ("for list") {
+      assert(P05.reverse(List[Int](1,2,3,4,5)).equals(List[Int](5,4,3,2,1)))
+      assert(P05.reverseByFoldRight(List[Int](1,2,3,4,5)).equals(List[Int](5,4,3,2,1)))
+    }
+    it ("for long list") {
+      assert(P05.reverse((1 to 10000).toList).equals((1 to 10000).toList.reverse))
+      assert(P05.reverseByFoldRight((1 to 10000).toList).equals((1 to 10000).toList.reverse))
+    }
+  }
+}
