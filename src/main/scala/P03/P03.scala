f0445b7ba74e7ca06bcbbc581ab2b8a48a9b15f8
UEDA Takeshi <takeshi@freemind.co.jp>
2016-06-10 07:53:52 +0900

    P03 implementation modified

--- src/main/scala/P03/P03.scala
+++ src/main/scala/P03/P03.scala
@@ -4,9 +4,9 @@ package P03
   * Created by tksued on 2016/05/28.
   */
 object P03 {
-  def nth[T](n: Int, list: List[T]): T = (n, list) match {
-    case (0, x::_) => x
-    case (h, _::xs) => nth(h - 1, xs)
-    case (_, Nil) => throw new NoSuchElementException
+  def nth[T](n: Int, list: List[T]): T = list match {
+    case x::_ if n == 0 => x
+    case _::xs if n > 0 => nth(n - 1, xs)
+    case _ => throw new NoSuchElementException
   }
 }
