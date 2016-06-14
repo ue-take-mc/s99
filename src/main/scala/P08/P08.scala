d3da3bf90d902e215ce526b039e9fc71c0377de1
UEDA Takeshi <takeshi@freemind.co.jp>
2016-06-14 15:07:17 +0900

    P08 modified AND P08 test by scalacheck

--- src/main/scala/P08/P08.scala
+++ src/main/scala/P08/P08.scala
@@ -4,22 +4,22 @@ package P08
   * Created by ueda on 2016/06/13.
   */
 object P08 {
-  def compressWithTailRecursive(list: List[Symbol]) : List[Symbol] = {
-    def compressFromTail(result : List[Symbol], xs: List[Symbol]): List[Symbol] = xs match {
-      case x :: tail => compressFromTail(x :: tail, tail.dropWhile(e => e == x))
-      case Nil => xs.reverse
+  def compressWithTailRecursive(list: List[Int]) : List[Int] = {
+    def compressFromTail(result : List[Int], xs: List[Int]): List[Int] = xs match {
+      case x :: tail => compressFromTail(x :: result, tail.dropWhile(e => e == x))
+      case Nil => result.reverse
     }
     compressFromTail(Nil, list)
   }
 
-  def compressWithFoldRight(list: List[Symbol]) : List[Symbol] = {
-    list.foldRight(List[Symbol]())((x, l) => {
+  def compressWithFoldRight(list: List[Int]) : List[Int] = {
+    list.foldRight(List[Int]())((x, l) => {
       if (l.isEmpty || (l.head != x)) x :: l
       else l
     })
   }
 
-  def compress(list: List[Symbol]) : List[Symbol] = list match {
+  def compress(list: List[Int]) : List[Int] = list match {
     case Nil => Nil
     case x :: xs => x :: compress(xs.dropWhile(e => x == e))
   }
--- src/test/scala/P08/P08Check.scala
+++ src/test/scala/P08/P08Check.scala
@@ -1,14 +1,14 @@
 package P08
 
-import org.scalacheck.{Arbitrary, Gen, Prop, Properties}
+import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 
 /**
   * Created by ueda on 2016/06/13.
   */
 class P08Check extends Properties("P08") {
-  def duplicates(list: List[Symbol]) : Int = {
-    def loop(list: List[Symbol], curr: Symbol, count: Int) : Int = list match {
+  def duplicates(list: List[Int]) : Int = {
+    def loop(list: List[Int], curr: Int, count: Int) : Int = list match {
       case Nil => count
       case x::xs =>
         if (curr == x) loop(xs, curr, count + 1) else loop (xs, x, count)
@@ -19,15 +19,15 @@ class P08Check extends Properties("P08") {
     }
   }
 
-  property("compress()") = {
-    val gen = for {
-      x <- Gen.choose[Symbol]('a, 'z)
-      y <- Gen.choose(1, 10)
-      s <- Gen.listOfN(y, implicitly[Arbitrary[Symbol]].arbitrary)
-    } yield s
+  property("compress()") = forAll{ (s: List[Int]) =>
+    P08.compress(s).length == s.length - duplicates(s)
+  }
 
-    Prop.forAll(gen) { case (x) =>
-      P08.compress(x).length == x.length - duplicates(x)
-    }
+  property("compressWithTailRecursive()") = forAll{ (s: List[Int]) =>
+    P08.compressWithTailRecursive(s).length == s.length - duplicates(s)
+  }
+
+  property("compressWithFoldRight()") = forAll{ (s: List[Int]) =>
+    P08.compressWithFoldRight(s).length == s.length - duplicates(s)
   }
 }
